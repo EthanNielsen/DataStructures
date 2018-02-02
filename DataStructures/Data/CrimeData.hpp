6d557cde7d031cdd39bcf450df48de8585dd4e63
EthanNielsen <31518851+EthanNielsen@users.noreply.github.com>
2018-02-02 11:51:52 -0700

    Created the Datatructure app and added the nessisary files.

---
+++ DataStructures/Data/CrimeData.hpp
@@ -0,0 +1,14 @@
+//
+//  CrimeData.hpp
+//  DataStructures
+//
+//  Created by Nielsen, Ethan on 2/2/18.
+//  Copyright © 2018 Ctech. All rights reserved.
+//
+
+#ifndef CrimeData_hpp
+#define CrimeData_hpp
+
+#include <stdio.h>
+
+#endif /* CrimeData_hpp */
---
+++ DataStructures/Runner.cpp
@@ -0,0 +1,9 @@
+//
+//  Runner.cpp
+//  DataStructures
+//
+//  Created by Nielsen, Ethan on 2/2/18.
+//  Copyright © 2018 Ctech. All rights reserved.
+//
+
+#include <stdio.h>
