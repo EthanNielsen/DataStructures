48dff38a54d28fc7275bd583bde8e31726bf2a49
EthanNielsen <31518851+EthanNielsen@users.noreply.github.com>
2018-02-02 12:23:17 -0700

    finished creating the FileController.cpp and .hpp checking the data.

--- DataStructures/Controller/FileController.cpp
+++ DataStructures/Controller/FileController.cpp
@@ -10,5 +10,34 @@
 
 vector<CrimeData> FileController :: readCrimeDataToVector(string filename)
 {
-    std :: vector<CrimeData> crimeVector
+    std :: vector<CrimeData> crimeVector;
+    string currentCSVLine;
+    int rowCount = 0;
+    
+    ifstream dataFile(filename)
+    
+    //Checks if the file is at the same path.
+    if (datafile.is_open())
+    {
+        while (!dataFile.eof());
+        {
+            getline(dataFile, currentCSVLine, '\n');
+            
+            if (rowCount != 0)
+            {
+                if (currentCSVLine.length() != 0)
+                {
+                    CrimeData row(currentCSVLine);
+                    crimeVector.push_back(row);
+                }
+            }
+            rowCount++;
+        }
+        dataFile.close();
+    }
+    else
+    {
+        cerr << "NO FILE" << endl;
+    }
+    return crimeVector;
 }
