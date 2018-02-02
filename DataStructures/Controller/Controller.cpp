ee6c7734e59b69a24464513b64e95ef14091586e
EthanNielsen <31518851+EthanNielsen@users.noreply.github.com>
2018-02-02 12:08:21 -0700

    created the code for the conroller.cpp (Finds largest and smallest Crime data).

--- DataStructures/Controller/Controller.cpp
+++ DataStructures/Controller/Controller.cpp
@@ -7,3 +7,39 @@
 //
 
 #include "Controller.hpp"
+
+using namespace std;
+
+// The code below sorts through all the Crime Data and finds the larges and smallest part of it.
+void Controller :: start()
+{
+    findMaxAndMin();
+    
+    
+}
+
+void Controller :: findMaxAndMin()
+{
+    Timer searchTimer;
+    searchTimer.startTimer();
+    vector<CrimeData> myData = FileController :: readCrimeDataToVector("/Users/enie1762/Documents/CS20420/DataStructures/DataStructures/Data/crime.csv");
+    
+    int minIndex = 0;
+    int maxIndex = 0;
+    
+    for (int index = 1; index < myData.size(); index++)
+    {
+        if (myData [minIndex] > myData [index])
+        {
+            minIndex = index;
+        }
+        else if (myData [maxIndex] < myData [index])
+        {
+            maxIndex = index;
+        }
+    }
+    searchTimer.stopTimer();
+    cout << "The smallest Crime stat is at " << minIndex << " and it is: " << myData[minIndex] << endl;
+    cout << "The largest Crime stat is at " << maxIndex << " and it is: " << myData[maxIndex] << endl;
+    searchTimer.displayInformation();
+}
