5da5da06681389d496cb39e5fa86febfccb23985
EthanNielsen <31518851+EthanNielsen@users.noreply.github.com>
2018-02-26 12:15:43 -0700

    created the TestingStacksAndQueues hpp and cpp files

---
+++ DataStructures/Testers/TestingStacksAndQueues.cpp
@@ -0,0 +1,9 @@
+//
+//  TestingStacksAndQueues.cpp
+//  DataStructures
+//
+//  Created by Nielsen, Ethan on 2/26/18.
+//  Copyright © 2018 Ctech. All rights reserved.
+//
+
+#include "TestingStacksAndQueues.hpp"
---
+++ DataStructures/Testers/TestingStacksAndQueues.hpp
@@ -0,0 +1,23 @@
+//
+//  TestingStacksAndQueues.hpp
+//  DataStructures
+//
+//  Created by Nielsen, Ethan on 2/26/18.
+//  Copyright © 2018 Ctech. All rights reserved.
+//
+
+#ifndef TestingStacksAndQueues_hpp
+#define TestingStacksAndQueues_hpp
+
+#include "../Controller/Tools/Timer.hpp"
+#include "../Controller/FileController.hpp"
+#include "../Model/Structures/Linear/Queue.hpp"
+#include "../Model/Structures/Linear/Stack.hpp"
+
+#include <iostream>
+
+using namespace std;
+
+
+
+#endif /* TestingStacksAndQueues_hpp */
