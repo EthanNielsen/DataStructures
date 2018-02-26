44c17480273a3ee52581038f2f05aadd684dec5e
EthanNielsen <31518851+EthanNielsen@users.noreply.github.com>
2018-02-26 12:25:48 -0700

    added code that should compare the list and Stack types by using ints.

--- DataStructures/Testers/TestingStacksAndQueues.cpp
+++ DataStructures/Testers/TestingStacksAndQueues.cpp
@@ -11,17 +11,44 @@
 template <class Type>
 class stackVersusList
 {
+    Stack<int> numberStack;
+    LinkedList<int> numberList;
+    Timer stackTimer, listTimer;
     
+    for (int index = 0; index < 10; index++)
+    {
+        numberStack.add(index);
+        numberList.add(index);
+    }
+    
+    cout << "Get the third from the last item from the structure" << endl;
+    
+    int index = 0;
+    int value = -99;
+    stackTimer.startTime();
+    
+    while (index < 7)
+    {
+        cout << numberStack.pop() << endl;
+        
+        index++;
+    }
+    
+    stackTimer.stopTimer();
+    index = 0;
+    listTimer.startTimer();
+    value = numbersList.geFromindex(7);
+    listTimer.stopTimer();
 }
 
 template <class Type>
 class stackVersusQueue
 {
     
-}
+};
 
 template <class Type>
 class stackVersusQueue
 {
     
-}
+};
