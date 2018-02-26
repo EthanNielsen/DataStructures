03aed074bc47b43b98cace85c6ea762729d051c2
EthanNielsen <31518851+EthanNielsen@users.noreply.github.com>
2018-02-26 12:44:22 -0700

    added the constructor for CircularList

--- DataStructures/Model/Structures/Linear/CircularList.hpp
+++ DataStructures/Model/Structures/Linear/CircularList.hpp
@@ -32,4 +32,16 @@ public:
     int getSize() const;
 };
 
+template <class Type>
+CircularList<Type> :: CircularList()
+{
+    front->setPrevious(end);
+    front->setNext(end);
+    
+    end->setPrevious(front);
+    end->setNext(front);
+}
+
+
+
 #endif /* CircularList_hpp */
