17361834a3a10688a83cad4227f30801e7b499ce
EthanNielsen <31518851+EthanNielsen@users.noreply.github.com>
2018-03-19 12:27:55 -0600

    added the unlet Graph and added a Graph.hpp into the NonLinear folder.

---
+++ DataStructures/Model/Structures/NonLinear/Graph.h
@@ -0,0 +1,57 @@
+//
+//  Graph.h
+//  DataStructures
+//
+//  Created by Nielsen, Ethan on 3/19/18.
+//  Copyright © 2018 Ctech. All rights reserved.
+//
+
+#ifndef Graph_h
+#define Graph_h
+
+#include <queue>
+#include <assert.h>
+
+using namespace std;
+template <class Type>
+class Graph
+{
+private:
+    static const int MAXIMUM = 20;
+    bool adjacencyMatrix [MAXIMUM][MAXIMUM];
+    int weightCostMatrix [MAXIMUM][MAXIMUM];
+    Type graphData[MAXIMUM];
+    int vertexCount;
+    void depthFirstTraversal(Graph<Type> &  graph, int vertex, bool markeVertices[]);
+public:
+    Graph();
+    //Graph operations
+    
+    void addVertex(const Type& value);
+    
+    //Connect vertices
+    void addEdge(int source, int target);
+    void addEdgeUndirected(int source, int target);
+    void addEdgeCost(int source, int target, int cost);
+    //Disconnect vertices
+    void removeEdge(int source, int target);
+    void removeEdgeUndirected(int source, int target);
+    void removeEdgeCost(int source, int target);
+    
+    //Accessors
+    Type& operator [] (int vertex);
+    Type operator [] (int vertex) const;
+    int size() const;
+    
+    //Check connections
+    bool hasUndirectedConnection(int source, int target) const;
+    bool areConnected(int source, int target) const;
+    std::set<int> neighbors(int vertex) const;
+    
+    //Traversals
+    void depthFirstTraversal(Graph<Type> & graph, int vertex);
+    void breadthFirstTraversal(Graph<Type> & graph, int vertex);
+    int costTraversal(Graph<Type> & graph, int vertex);
+}
+
+#endif /* Graph_h */
