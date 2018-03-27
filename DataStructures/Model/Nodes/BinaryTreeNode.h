fa5d22acd00b16784604465e084b0857a8b49b2a
EthanNielsen <31518851+EthanNielsen@users.noreply.github.com>
2018-03-27 12:38:18 -0600

    added the BinaryTreeNode header file.

---
+++ DataStructures/Model/Nodes/BinaryTreeNode.h
@@ -0,0 +1,66 @@
+//
+//  BinaryTreeNode.h
+//  DataStructures
+//
+//  Created by Nielsen, Ethan on 3/27/18.
+//  Copyright © 2018 Ctech. All rights reserved.
+//
+
+#ifndef BinaryTreeNode_h
+#define BinaryTreeNode_h
+
+template <class Type>
+class BinaryTreeNode : public Node<Type>
+{
+private:
+    BinaryTreeNode<Type> * root;
+    BinaryTreeNode<Type> * right;
+    BinaryTreeNode<Type> * left;
+
+public:
+    BinaryTreeNode();
+    BinaryTreeNode(Type data);
+    
+    void setRootNode(BinaryTreeNode<Type> * root);
+    void setRightNode(BinaryTreeNode<Type> * right);
+    void setLeftNode(BinaryTreeNode<Type> * left);
+    
+    BinaryTreeNode<Type> * getRightNode();
+    BinaryTreeNode<Type> * getRightNode();
+    BinaryTreeNode<Type> * getRightNode();
+
+};
+
+template <class Type>
+BinaryTreeNode<Type> :: BinaryTreeNode() : Node<Type>()
+{
+    root = nullptr;
+    left = nullptr;
+    right = nullptr;
+}
+
+template <class Type>
+BinaryTreeNode<Type> :: BinaryTreeNode(Type data) : Node<Type>(data)
+{
+    root = nullptr;
+    left = nullptr;
+    right = nullptr;
+}
+
+template <class Type>
+BinaryTreeNode<Type> * BinaryTreeNode<Type> :: getRootNode()
+{
+    return this->root;
+}
+
+template <class Type>
+BinaryTreeNode<Type> * BinaryTreeNode<Type> :: getRightNode()
+{
+    return this->right;
+}
+
+
+
+
+
+#endif /* BinaryTreeNode_h */
---
+++ DataStructures/Model/Nodes/N_AryTreeNode.h
@@ -0,0 +1,13 @@
+//
+//  N_AryTreeNode.h
+//  DataStructures
+//
+//  Created by Nielsen, Ethan on 3/27/18.
+//  Copyright © 2018 Ctech. All rights reserved.
+//
+
+#ifndef N_AryTreeNode_h
+#define N_AryTreeNode_h
+
+
+#endif /* N_AryTreeNode_h */
