a0cb928a634f3f0855db45233bf58afd44c23dc1
EthanNielsen <31518851+EthanNielsen@users.noreply.github.com>
2018-03-27 13:43:47 -0600

    Added the Tree header file and cpp file

---
+++ DataStructures/Testers/BinaryTreeTester.cpp
@@ -0,0 +1,9 @@
+//
+//  BinaryTreeTester.cpp
+//  DataStructures
+//
+//  Created by Nielsen, Ethan on 3/27/18.
+//  Copyright © 2018 Ctech. All rights reserved.
+//
+
+#include "BinaryTreeTester.hpp"
---
+++ DataStructures/Testers/BinaryTreeTester.hpp
@@ -0,0 +1,23 @@
+//
+//  BinaryTreeTester.hpp
+//  DataStructures
+//
+//  Created by Nielsen, Ethan on 3/27/18.
+//  Copyright © 2018 Ctech. All rights reserved.
+//
+
+#ifndef BinaryTreeTester_hpp
+#define BinaryTreeTester_hpp
+
+#include "../Model/Nodes/BinaryTreeNode.h"
+#include <iostream>
+
+class BinaryTreeTester
+{
+private:
+    
+public:
+    
+};
+
+#endif /* BinaryTreeTester_hpp */
