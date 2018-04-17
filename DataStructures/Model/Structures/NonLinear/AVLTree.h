abbcdbb07e1897a7d429686e639af5ff9d6ae324
EthanNielsen <31518851+EthanNielsen@users.noreply.github.com>
2018-04-17 11:57:33 -0600

    Added the BinaryTreeNode constructor and began on the heightDifference method.

---
+++ DataStructures/Model/Structures/NonLinear/AVLTree.h
@@ -0,0 +1,45 @@
+//
+//  AVLTree.h
+//  DataStructures
+//
+//  Created by Nielsen, Ethan on 4/17/18.
+//  Copyright © 2018 Ctech. All rights reserved.
+//
+
+#ifndef AVLTree_h
+#define AVLTree_h
+
+template <class Type>
+class AVLTree : public BinarySearchTree<Type>
+{
+private:
+    
+    BinaryTreeNode<Type> * leftRotation (BinaryTreeNode<Type> * parent);
+    BinaryTreeNode<Type> * rightRotation(BinaryTreeNode<Type> * parent);
+    BinaryTreeNode<Type> * leftRightRotation(BinaryTreeNode<Type> * parent);
+    BinaryTreeNode<Type> * rightLeftRotation(BinaryTreeNode<Type> * parent);
+ 
+    BinaryTreeNode<Type> * balanceSubTree(BinaryTreeNode<Type> * parent);
+    
+    BinaryTreeNode<Type> * insertNode(BinaryTreeNode<Type> * parent, Type value);
+    BinaryTreeNode<Type> * removeNode(BinaryTreeNode<Type> * parent, Type value);
+    
+    int heightDifference(BinaryTreeNode<Type> * parent);
+    
+public:
+    AVLTree();
+    
+    void insert(Type itemToInsert);
+    void remove(Type value);
+};
+
+/*
+ A negative value means the right is greater than the left
+ */
+template <class Type>
+int AVLTree<Type> :: heightDifference(BinaryTreeNode<Type> * node)
+{
+    int balance;
+    
+}
+#endif /* AVLTree_h */
